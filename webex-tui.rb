b784511ae315f4e66cf8bbf975bb334f35bffcaf
sgrimee <sgrimee@gmail.com.me>
2023-10-13 18:30:07 +0200

    build: from source instead of precompiled

--- webex-tui.rb
+++ webex-tui.rb
@@ -1,11 +1,14 @@
 class WebexTui < Formula
   desc "Unofficial Webex chat client for the terminal"
   homepage "https://github.com/sgrimee/webex-tui"
-  url "https://github.com/sgrimee/webex-tui/releases/download/v0.1.0/webex-tui-aarch64-apple-darwin.tar.gz"
-  sha256 "a194db07126c03cd95a270cd446d66f40e68c63ef88bfa2e2016d9b0796311f3"
+  url "https://github.com/sgrimee/webex-tui/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 ""
   license "MIT"
 
+  depends_on "rust" => :build
+  
   def install
+    system "cargo", "build", "--release", "--bin", "webex-tui"  
     bin.install "webex-tui"
   end
 end
