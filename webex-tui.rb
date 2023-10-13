cca1b8e20991b461c1b7ea0a2d03c84d32211747
sgrimee <sgrimee@gmail.com.me>
2023-10-13 18:40:08 +0200

    build: add sha256

--- webex-tui.rb
+++ webex-tui.rb
@@ -2,7 +2,7 @@ class WebexTui < Formula
   desc "Unofficial Webex chat client for the terminal"
   homepage "https://github.com/sgrimee/webex-tui"
   url "https://github.com/sgrimee/webex-tui/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 ""
+  sha256 "f5779addd1d6f564bd404f445410fbb0d3e0c478520ad9961a40712107c01e93"
   license "MIT"
 
   depends_on "rust" => :build
