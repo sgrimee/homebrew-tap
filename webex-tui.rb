b1d1bbc7586abd06946bc619973310a70f7506c2
Sam Grimee - LX2SG <sgrimee@users.noreply.github.com>
2023-10-16 16:19:42 +0200

    Release 0.1.2

--- webex-tui.rb
+++ webex-tui.rb
@@ -1,8 +1,8 @@
 class WebexTui < Formula
   desc "Unofficial Webex chat client for the terminal"
   homepage "https://github.com/sgrimee/webex-tui"
-  url "https://github.com/sgrimee/webex-tui/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "1cc91bd83f9e04ba2cd8c5d8abb7431ece81c61a69a9c1511cf2f6e0cfddbc0a"
+  url "https://github.com/sgrimee/webex-tui/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "b57ad1b6477ff143b7cc34f53062e9ae80b6d11aee4c1e97ac3ec7b4574059ef"
   license "MIT"
 
   depends_on "rust" => :build
