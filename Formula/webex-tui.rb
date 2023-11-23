9ee3e284d0e9df284fa7791895bb33ccd83daced
Sam Grimee - LX2SG <sgrimee@users.noreply.github.com>
2023-11-23 18:00:48 +0100

    build: testing workflow

--- Formula/webex-tui.rb
+++ Formula/webex-tui.rb
@@ -1,5 +1,5 @@
 class WebexTui < Formula
-  desc "Unofficial Webex chat client for the terminal"
+  desc "Unofficial minimal Webex chat client for the terminal"
   homepage "https://github.com/sgrimee/webex-tui"
   url "https://github.com/sgrimee/webex-tui/archive/refs/tags/v0.4.5.tar.gz"
   sha256 "fa25c0822e8d3c7e44160a034ddd634c2c2dc7d0fcefb34cfcd546233f75a2b9"
